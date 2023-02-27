a8a809ed39549cad12a00d078595d14c7d5e2eeb
Hubert Krauza <hubertkrauza@gmail.com>
2023-02-27 17:27:46 +0100

    refactor my printf

--- lab1/main.py
+++ lab1/main.py
@@ -5,16 +5,19 @@ import sys
 def my_printf(format_string,param):
     #print(format_string)
     shouldDo = True
+    string_var = ""
     for idx in range(0,len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param, end="")
+                string_var = string_var + str(param)
+                # print(param, end="")
                 shouldDo=False
             else:
-                print(format_string[idx],end="")
+                # print(format_string[idx],end="")
+                string_var = string_var + str(format_string[idx])
         else:
             shouldDo=True
-    print("")
+    print(string_var)
 
 data=sys.stdin.readlines()
 
