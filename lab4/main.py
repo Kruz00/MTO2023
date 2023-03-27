8b676b5a0995b00cc83f7d1d2dfbcbba6fd8b93a
Hubert Krauza <hubertkrauza@gmail.com>
2023-03-27 16:43:43 +0200

    lab4 1 test

--- lab4/main.py
+++ lab4/main.py
@@ -18,9 +18,7 @@ def my_printf(format_string, param):
     print("")
 
 
-# data = sys.stdin.readlines()
-data = ["abc #ghufrhhf",
-        "00123400"]
+data = sys.stdin.readlines()
 
 for i in range(0, len(data), 2):
     my_printf(data[i].rstrip(), data[i + 1].rstrip())
