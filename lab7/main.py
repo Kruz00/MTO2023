dc20b838ab29dc85537dbd14bd15fc2dc31a004f
Hubert Krauza <hubertkrauza@gmail.com>
2023-04-24 16:47:37 +0200

    lab7 change to pyython

--- lab7/main.py
+++ lab7/main.py
@@ -7,9 +7,10 @@ def my_printf(format_string, param):
     shouldDo = True
     for idx in range(0, len(format_string)):
         if shouldDo:
-            if format_string[idx] == '#' and format_string[idx + 1] == 'g':
-                number = int(str(int(param))[::-1])
-                print(number, end="")
+            if format_string[idx] == '#' and format_string[idx + 1] == 'j':
+                number = int(param)
+                out_str = '{:x}'.format(number)
+                print(out_str, end="")
                 shouldDo = False
             else:
                 print(format_string[idx], end="")
@@ -19,8 +20,8 @@ def my_printf(format_string, param):
 
 
 # data = sys.stdin.readlines()
-data = ["abc #7.3kjhdf#.10kkjdf df#7kdf",
-        "TEST2"]
+data = ["abc #100",
+        "63"]
 
 for i in range(0, len(data), 2):
     my_printf(data[i].rstrip(), data[i + 1].rstrip())
