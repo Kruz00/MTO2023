916b10906e04c19c397b8d64cc03cec4c4e6aff0
Hubert Krauza <hubertkrauza@gmail.com>
2023-04-24 16:55:02 +0200

    lab7 done impl

--- lab7/main.py
+++ lab7/main.py
@@ -9,7 +9,13 @@ def my_printf(format_string, param):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx + 1] == 'j':
                 number = int(param)
-                out_str = '{:x}'.format(number)
+                out_str = '{:x}'.format(number)\
+                    .replace("a", "g")\
+                    .replace("b", "h")\
+                    .replace("c", "i")\
+                    .replace("d", "j")\
+                    .replace("e", "k")\
+                    .replace("f", "l")
                 print(out_str, end="")
                 shouldDo = False
             else:
@@ -20,8 +26,8 @@ def my_printf(format_string, param):
 
 
 # data = sys.stdin.readlines()
-data = ["abc #100",
-        "63"]
+data = ["abc #j",
+        "10"]
 
 for i in range(0, len(data), 2):
     my_printf(data[i].rstrip(), data[i + 1].rstrip())
