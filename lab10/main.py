86c26adfd6e1176a2a87a3186cf149160217f219
Hubert Krauza <hubertkrauza@gmail.com>
2023-05-29 17:52:25 +0200

    lab10 init

---
+++ lab10/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+
+
+def my_printf(format_string, param):
+    shouldDo = True
+    for idx in range(0, len(format_string)):
+        if shouldDo:
+            if format_string[idx] == '#' and format_string[idx + 1] == 'a':
+
+                number = int(param)
+                number_of_digits = len(str(abs(number)))
+
+                final_number = int((number*2)/number_of_digits)
+
+                if number % 2 == 0:
+                    out_str = str(final_number)
+                else:
+                    out_str = '{:x}'.format(number)
+
+                print(out_str, end="")
+                shouldDo = False
+            else:
+                print(format_string[idx], end="")
+        else:
+            shouldDo = True
+    print("")
+
+
+data = sys.stdin.readlines()
+# data = ["abc #j",
+#         "10"]
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i + 1].rstrip())
