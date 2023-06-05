f5c0e10b45c2d0b837912c0c4d6ad94899184cf2
Hubert Krauza <hubertkrauza@gmail.com>
2023-06-05 17:57:18 +0200

    lab11 added transformation

--- lab11/main.py
+++ lab11/main.py
@@ -3,6 +3,8 @@
 import sys
 import re
 
+alphabet = 'abcdefghij'
+
 
 def my_printf(format_string, param):
     matcher = re.search("#b", format_string)
@@ -11,17 +13,24 @@ def my_printf(format_string, param):
         return
 
     number = int(param)
-    if number == 0:
-        out = format_string.replace(matcher.group(), '0')
-    else:
-        out = format_string.replace(matcher.group(), str(number))
+    out_decimal_reversed = bin(number).replace('0b', '')[::-1]
+    out_transformed = ''
+
+    for i in range(len(out_decimal_reversed)):
+        if out_decimal_reversed[i] == '0':
+            out_transformed += '0'
+        else:
+            out_transformed += alphabet[i % len(alphabet)]
+
+    out_param = out_transformed[::-1]
+    out = format_string.replace(matcher.group(), out_param)
 
     print(out)
 
 
 # data = sys.stdin.readlines()
 data = ["abc #b",
-        "16"]
+        "123"]
 
 for i in range(0, len(data), 2):
     my_printf(data[i].rstrip(), data[i + 1].rstrip())
