1275dce95fc6bccad97a2f7cd1dc3d6c0a3caef9
Hubert Krauza <hubertkrauza@gmail.com>
2023-06-05 17:27:52 +0200

    lab11 init with quick implementation

---
+++ lab11/main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+
+def transform(number, length):
+    return int((number * 2) / length)
+
+
+def my_printf(format_string, param):
+    matcher = re.search("#b", format_string)
+    if not matcher:
+        print(format_string)
+        return
+
+    number = int(param)
+    if number == 0:
+        out = format_string.replace(matcher.group(), '0')
+    else:
+        out = format_string.replace(matcher.group(), str(number))
+
+    print(out)
+
+
+# data = sys.stdin.readlines()
+data = ["abc #b",
+        "16"]
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i + 1].rstrip())
